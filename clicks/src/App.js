53425f0bc52b7e7d01777831b28c92560e801694
Cecilia Biagiola <biagiolacecilia@gmail.com>
2022-12-06 18:09:57 -0300

    full responsive design added

--- clicks/src/App.js
+++ clicks/src/App.js
@@ -3,7 +3,8 @@ import {Counter} from './Counter/index.js'
 import {Button} from './Button/index.js'
 import { useState } from 'react';
 import Boludos from './img/boludos.jpg';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
+
  
 
 function App() {
@@ -34,25 +35,27 @@ function App() {
     setNumClicks(0);  
   };
   return (
-   <div className='app'>
-      <div className='title-img'>
-        <h1 className='main-title'>Contador de Boludos</h1>
-        <img 
-          src={Boludos}
-          alt='Atendedor de boludos'
-          className='img-boludos'/>
-        </div>
-      <Counter
-        numClicks= {numClicks} />
-      <Button
-        text='Otro Boludo'
-        clickButton={true}
-        onHandleClick={ onHandleClick } /> 
-      <Button
-        text='Reset'
-        clickButton={false}
-        onHandleClick={ resetCounter } /> 
-   </div>
+    <div className='app'>
+      
+        <div className='title-img'>
+          <img 
+            src={Boludos}
+            alt='Atendedor de boludos'
+            className='img-boludos'/>
+          <h1 className='main-title'>Contador de Boludos</h1>
+          </div>
+        <Counter
+          numClicks= {numClicks} />
+        <Button
+          text='Otro Boludo'
+          clickButton={true}
+          onHandleClick={ onHandleClick } /> 
+        <Button
+          text='Reset'
+          clickButton={false}
+          onHandleClick={ resetCounter } /> 
+      
+ </div>
     
   );
 }
